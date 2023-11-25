9565837253e006e60566a3b5e1ce881fd0fd8149
jonathanvw <jonathanvwersch@gmail.com>
2023-11-25 15:53:09 +0000

    feature: add functions for geo stuff

--- bus-stop-display/src/App.js
+++ bus-stop-display/src/App.js
@@ -4,45 +4,50 @@ import CurrentStop from "./components/CurrentStop"; // Adjust the path as necess
 import { MapContainer, TileLayer, Marker } from "react-leaflet"; //Marker, useMapEvents
 import TwoOhFive from "./data/routes/205.json";
 import BusStops from "./data/custom/205.json";
+import "leaflet/dist/leaflet.css";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
 
 function App() {
   const line = TwoOhFive.TransXChange.Services.Service.Lines.Line;
+  const [showMap, setShowMap] = useState(false);
 
   return (
     <div className="App flex flex-col min-h-screen bg-white w-full">
       {/* RouteHeader sits above and is not vertically centered */}
       <RouteHeader
+        showMap={showMap}
+        onShowMap={() => setShowMap((prevState) => !prevState)}
         route={line.LineName}
         origin={line.OutboundDescription.Origin}
         destination={line.OutboundDescription.Destination}
       />
-      {/* Fullscreen Carousel */}
-      <div className="carousel w-full flex-1">
-        {/* Main View - Carousel */}
-        <div className="carousel-item w-full h-full">
-          {/* Container for CurrentStop, which takes up the remaining space */}
-          <div className="flex-1 flex items-center w-full">
-            <CurrentStop stopName="Ellic Close" />
-          </div>
-        </div>
-        {/* Map View - Carousel */}
-        <div className="carousel-item w-full h-full">
-          {/* Container for CurrentStop, which takes up the remaining space */}
-          <div className="flex-1 flex items-center w-full">
-            <div className="h-full w-full relative">
-              <MapContainer
-                center={[BusStops.custom[0].lat, BusStops.custom[0].long]}
-                zoom={14}
-                style={{ height: "100vh", width: "100vw" }}
-              >
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                {BusStops.custom.map((stop, index) => (
-                  <Marker key={index} position={[stop.lat, stop.long]} />
-                ))}
-              </MapContainer>
-            </div>
-          </div>
-        </div>
+
+      <div className="h-full w-full relative">
+        {showMap ? (
+          <MapContainer
+            center={[BusStops.custom[0].lat, BusStops.custom[0].long]}
+            zoom={14}
+            style={{ height: "100vh", width: "100vw" }}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            {BusStops.custom.map((stop, index) => (
+              <Marker
+                key={index}
+                position={[stop.lat, stop.long]}
+                icon={
+                  new Icon({
+                    iconUrl: markerIconPng,
+                    iconSize: [25, 41],
+                    iconAnchor: [12, 41],
+                  })
+                }
+              />
+            ))}
+          </MapContainer>
+        ) : (
+          <CurrentStop stopName="Ellic Close" />
+        )}
       </div>
     </div>
   );
