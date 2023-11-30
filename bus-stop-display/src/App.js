c0a56dbc0dc26c251465271f2a49dde329ec3066
Florence <fhinder@gmail.com>
2023-11-30 18:31:39 +0000

    adding gps tracking to the app and map click functionality

--- bus-stop-display/src/App.js
+++ bus-stop-display/src/App.js
@@ -1,32 +1,24 @@
-//// apps.js file
+//// app.js file
 
 // Import the required dependencies
 import React, { useState } from "react";
 import RouteHeader from "./components/RouteHeader"; // Adjust the path as necessary
 import CurrentStop from "./components/CurrentStop"; // Adjust the path as necessary
-import { MapContainer, TileLayer, Marker } from "react-leaflet"; //Marker, useMapEvents
+import MapContainer from "./components/MapContainer";
 import TwoOhFive from "./data/routes/205.json";
 import BusStops from "./data/custom/205.json";
 import "leaflet/dist/leaflet.css";
-import markerIconPng from "leaflet/dist/images/marker-icon.png";
-import { renderToStaticMarkup } from "react-dom/server";
-import { Bus } from "phosphor-react";
-import L from "leaflet";
 
 function App() {
   const line = TwoOhFive.TransXChange.Services.Service.Lines.Line;
   const [showMap, setShowMap] = useState(false);
 
-  // Create a custom icon using the Phosphor 'Bus' icon
-  const busIconMarkup = renderToStaticMarkup(<Bus size={24} />);
-  const busIcon = new L.Icon({
-    iconUrl: "data:image/svg+xml;base64," + btoa(busIconMarkup),
-    iconSize: [48, 48],
-    iconAnchor: [12, 48], // Adjust if needed
-  });
+  const geolocation = { lat: 51.18153, long: 0.38451 };
 
-  // Example bus location (replace with actual data)
-  const busLocation = { lat: 51.18153, long: 0.38451 };
+  //// Things to still add:
+  // - Current GPS coordinates
+  // - Route Line Displayed
+  // - Play Audio when within 150 ms
 
   return (
     <div className="App flex flex-col min-h-screen bg-white w-full">
@@ -41,32 +33,7 @@ function App() {
 
       <div className="h-full w-full relative">
         {showMap ? (
-          <MapContainer
-            center={[BusStops.custom[0].lat, BusStops.custom[0].long]}
-            zoom={14}
-            style={{ height: "100vh", width: "100vw" }}
-          >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {/* Add bus marker */}
-            <Marker
-              position={[busLocation.lat, busLocation.long]}
-              icon={busIcon}
-            />
-
-            {BusStops.custom.map((stop, index) => (
-              <Marker
-                key={index}
-                position={[stop.lat, stop.long]}
-                icon={
-                  new L.Icon({
-                    iconUrl: markerIconPng,
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                  })
-                }
-              />
-            ))}
-          </MapContainer>
+          <MapContainer BusStops={BusStops} geolocation={geolocation} />
         ) : (
           <CurrentStop stopName="Ellic Close" />
         )}
---
+++ bus-stop-display/src/components/MapContainer.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Bus } from "phosphor-react";
+import L from "leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+
+const busIconMarkup = renderToStaticMarkup(<Bus size={24} />);
+const busIcon = new L.Icon({
+  iconUrl: "data:image/svg+xml;base64," + btoa(busIconMarkup),
+  iconSize: [48, 48],
+  iconAnchor: [12, 48],
+});
+
+const HandleMapClick = ({ setCoordinates }) => {
+  useMapEvents({
+    click(e) {
+      const { lat, lng } = e.latlng;
+      setCoordinates({ lat: lat, long: lng });
+    },
+  });
+  return null; // Component does not render anything
+};
+
+const CustomMapContainer = ({ BusStops }) => {
+  const [geolocation, setGeolocation] = useState({
+    lat: 51.18153,
+    long: 0.38451,
+  });
+
+  const watchIdRef = useRef(null);
+  const MAX_READINGS = 10;
+  const readingsQueueRef = useRef([]);
+
+  useEffect(() => {
+    const updateLocation = (newLat, newLong) => {
+      setGeolocation({ lat: newLat, long: newLong });
+    };
+
+    if (navigator.geolocation) {
+      watchIdRef.current = navigator.geolocation.watchPosition(
+        (position) => {
+          const { latitude, longitude, accuracy } = position.coords;
+          if (!isNaN(latitude) && !isNaN(longitude)) {
+            const newReading = { latitude, longitude, accuracy };
+            readingsQueueRef.current.push(newReading);
+
+            if (readingsQueueRef.current.length > MAX_READINGS) {
+              readingsQueueRef.current.shift();
+            }
+
+            const sum = readingsQueueRef.current.reduce(
+              (acc, reading) => ({
+                latitude: acc.latitude + reading.latitude,
+                longitude: acc.longitude + reading.longitude,
+                accuracy: acc.accuracy + reading.accuracy,
+              }),
+              { latitude: 0, longitude: 0, accuracy: 0 }
+            );
+            const count = readingsQueueRef.current.length;
+            const averageLocation = {
+              latitude: sum.latitude / count,
+              longitude: sum.longitude / count,
+              accuracy: sum.accuracy / count,
+            };
+
+            updateLocation(averageLocation.latitude, averageLocation.longitude);
+          } else {
+            console.error("Invalid coordinates:", latitude, longitude);
+          }
+        },
+        (error) => {
+          console.error("Error retrieving location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+
+    return () => {
+      if (watchIdRef.current) {
+        navigator.geolocation.clearWatch(watchIdRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <MapContainer
+      center={[BusStops.custom[0].lat, BusStops.custom[0].long]}
+      zoom={14}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <HandleMapClick setCoordinates={setGeolocation} />
+      <Marker position={[geolocation.lat, geolocation.long]} icon={busIcon} />
+      {BusStops.custom.map((stop, index) => (
+        <Marker
+          key={index}
+          position={[stop.lat, stop.long]}
+          icon={
+            new L.Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        />
+      ))}
+    </MapContainer>
+  );
+};
+
+export default CustomMapContainer;
