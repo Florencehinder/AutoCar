9f3464fd3b1eec21776e77418553cec3cc556a0a
jonathanvw <jonathanvwersch@gmail.com>
2023-12-20 13:47:50 +0100

    add line coordinates

--- bus-stop-display/src/components/MapContainer.js
+++ bus-stop-display/src/components/MapContainer.js
@@ -3,7 +3,6 @@ import { Bus } from "phosphor-react";
 import L from "leaflet";
 import "leaflet.offline";
 import { renderToStaticMarkup } from "react-dom/server";
-import { lineCoordinates } from "../data/custom/205/line_coordinates.js";
 import {
   MapContainer,
   TileLayer,
@@ -45,6 +44,7 @@ const CustomMapContainer = ({
   onLocationUpdate,
   clickOrGps,
   geoLocation,
+  lineCoordinates,
 }) => {
   const [map, setMap] = useState();
   const [zoomLevel, setZoomLevel] = useState(16); // Initialize zoom level state
